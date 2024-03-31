0097faab2c364c6c6717f528f68d8444c3f09ce6
Andrei Kryvulia <andrei.k@datylon.com>
2024-03-31 14:22:38 +0300

    app.module changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EndpointsModule } from './endpoints/endpoints.module';
+import { AuthModule } from './endpoints/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EndpointsModule],
+  imports: [
+    EndpointsModule,
+    AuthModule,
+    // to have access to .env file through ConfigService
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
