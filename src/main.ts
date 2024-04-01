fa414708d346130414bbbd017fafba7885716cc2
Andrei Kryvulia <andrei.k@datylon.com>
2024-04-01 11:16:32 +0300

    logging service, logging middleware embedded

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,49 @@
-import { NestFactory } from '@nestjs/core';
+import {HttpAdapterHost, NestFactory} from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { parse } from 'yaml';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
+import {CustomExceptionFilter} from "./services/exception-filter/exception.filter";
+import {ConfigService} from "@nestjs/config";
+import {LoggingService} from "./services/logging/logging.service";
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const filePath = join(__dirname, '..', 'doc', 'api.yaml');
-  const file = await readFile(filePath, 'utf-8');
-  const swaggerDocument = parse(file);
-  SwaggerModule.setup('doc', app, swaggerDocument);
+  // bind logging service
+  const loggingService = await app.resolve(LoggingService)
+  app.useLogger(loggingService)
 
-  const PORT = Number(process.env.PORT || 4000);
+  // apply custom exception filter globally
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CustomExceptionFilter(httpAdapterHost, loggingService));
+
+  try {
+    const filePath = join(__dirname, '..', 'doc', 'api.yaml');
+    const file = await readFile(filePath, 'utf-8');
+    const swaggerDocument = parse(file);
+    SwaggerModule.setup('doc', app, swaggerDocument);
+  } catch (error) {
+    await loggingService.log(`swagger api documentation is not configured, ${error}`)
+  }
+
+  const configService = app.get(ConfigService);
+
+  const PORT = Number(configService.get('PORT') || 4000);
   await app.listen(PORT);
+
+  process.on('uncaughtException', (error) => {
+    loggingService.log('uncaughtException')
+    loggingService.error(error)
+  })
+
+  process.on('unhandledRejection', (error) => {
+    loggingService.log('unhandledRejection')
+    loggingService.error(error)
+  })
 }
 bootstrap();
